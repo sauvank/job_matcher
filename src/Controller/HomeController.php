6d51be539fdb7c58101c743ad2314a3ed5ac5508
Kevin <3721632-sauvanaud.kevin@users.noreply.gitlab.com>
2026-08-21 19:46:09 +0200

    refactor: use conventional controller directory

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\UI\Http\Controller;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
